fa5006fa0c6caa27285849c30d6043f81fd9cf8c
Kevin Patrick Westropp <highlanderkev@proton.me>
2024-02-15 21:03:23 -0700

    Changes: - fix random numbers on web

--- lib/src/random_number/random_number.dart
+++ lib/src/random_number/random_number.dart
@@ -1,20 +1,21 @@
 class RandomNumber {
   RandomNumber(
-      {required this.status,
-      required this.min,
-      required this.max,
+      {
+      //   required this.status,
+      // required this.min,
+      // required this.max,
       required this.random});
 
-  String status;
-  int min;
-  int max;
+  // String status;
+  // int min;
+  // int max;
   int random;
 
   factory RandomNumber.fromJson(List<dynamic> json) {
     return RandomNumber(
-        status: json[0]['status'],
-        min: json[0]['min'],
-        max: json[0]['max'],
-        random: json[0]['random']);
+        // status: json[0]['status'],
+        // min: json[0]['min'],
+        // max: json[0]['max'],
+        random: json[0]);
   }
 }
--- lib/src/random_number/random_number_service.dart
+++ lib/src/random_number/random_number_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:http/http.dart' as http;
 
@@ -8,8 +9,15 @@ class RandomNumberService {
   RandomNumberService();
 
   Future<RandomNumber> fetchRandomNumber() async {
-    final headers = {'Access-Control-Allow-Origin': '*'};
-    final uri = Uri.https('csrng.net', '/csrng/csrng.php');
+    final headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers':
+          'Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+    };
+    // final uri = Uri.https('csrng.net', '/csrng/csrng.php');
+    final uri = Uri.http('www.randomnumberapi.com', '/api/v1.0/random');
     final response = await http.get(uri, headers: headers);
 
     if (response.statusCode == 200) {
@@ -17,6 +25,12 @@ class RandomNumberService {
     } else {
       throw Exception('Failed to load Random Number');
     }
+  }
+
+  int generateRandomNumber() {
+    Random random = Random();
+    int randomNumber = random.nextInt(10000000);
+    return randomNumber;
   }
 }
 
